25808b5aaa66ae34281664b75e3989e2df33f35e
yehia <yehia.elkashif@yahoo.com>
2020-04-01 23:00:50 +0200

    hello

---
+++ app/src/main/java/com/example/babyneeds/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.babyneeds;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.babyItem)
+    EditText babyItem;
+    @BindView(R.id.itemQuantity)
+    EditText itemQuantity;
+    @BindView(R.id.itemColor)
+    EditText itemColor;
+    @BindView(R.id.itemSize)
+    EditText itemSize;
+    @BindView(R.id.saveButton)
+    Button saveButton;
+    @BindView(R.id.layout_id)
+    LinearLayout layoutId;
+    private AlertDialog.Builder Builder;
+    private AlertDialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                      //  .setAction("Action", null).show();
+                createPopDialog();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public void createPopDialog() {
+
+        Builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+        Builder.setView(view);
+        dialog=Builder.create();
+        dialog.show();
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.saveButton)
+    public void onClick() {
+    }
+}
